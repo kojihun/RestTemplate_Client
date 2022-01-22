932206c8967cd4b7f3b4707a4880bd94a2e1d0df
kojihun <38236367+kojihun@users.noreply.github.com>
2022-01-22 15:13:33 +0900

    Initial commit

---
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,34 @@
+package com.example.client.service;
+
+import java.net.URI;
+
+import com.example.client.dto.UserResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class RestTemplateService {
+
+    // localhost/api/server/hello
+    // response
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .encode().build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+}
